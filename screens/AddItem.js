fe4477c3b96419491f948c25dc4d9de311c8f43e
Bhavesh Joshi <34483890+gokulbhaveshjoshi@users.noreply.github.com>
2020-12-21 12:37:57 +0530

    added screen

---
+++ screens/AddItem.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import { Text } from "react-native";
+
+export class AddItem extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Add Item</Text>
+            </View>
+        )
+    }
+}
---
+++ screens/List.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import { Text, View } from "react-native";
+
+
+export class List extends Component {
+    render(){
+       return(
+           <View>
+               <Text>List</Text>
+           </View>
+       );
+    }
+}
